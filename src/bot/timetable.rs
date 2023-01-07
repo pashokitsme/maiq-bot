ee257a13d604fbed1bbaad25869d1c67def41969
pashokitsme <pashamail79@gmail.com>
2023-01-07 22:17:48 +0300

    Added uid to notify header

--- src/bot/timetable.rs
+++ src/bot/timetable.rs
@@ -11,7 +11,7 @@ pub fn get_formatted_snapshot(snapshot: &Snapshot) -> Result<HashMap<String, Str
     snapshot
       .groups
       .iter()
-      .map(|g| (g.name.clone(), display_group(&g)))
+      .map(|g| (g.name.clone(), display_group(&g, &snapshot.uid)))
       .collect::<HashMap<String, String>>(),
   )
 }
@@ -20,11 +20,11 @@ pub async fn format_timetable<'g>(group_name: &'g str, snapshot: &Snapshot) -> B
   snapshot
     .group(group_name)
     .ok_or(BotError::NoTimetable)
-    .map(|g| display_group(&g))
+    .map(|g| display_group(&g, &snapshot.uid))
 }
 
-fn display_group(group: &Group) -> String {
-  let mut res = format!("Расписание: <b>{}</b>\n\n", group.name);
+fn display_group(group: &Group, snapshot_uid: &String) -> String {
+  let mut res = format!("Расписание <b>{}</b>\t(<code>{}</code>)\n\n", group.name, snapshot_uid);
   group.lessons.iter().for_each(|l| res.push_str(&display_lesson(&l)));
   res
 }
