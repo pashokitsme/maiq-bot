e86adc80d9a67ee37acf4eedec2b40b7d4136386
pashokitsme <pashamail79@gmail.com>
2023-03-20 22:04:14 +0300

    Disable sleep log

--- src/poller.rs
+++ src/poller.rs
@@ -6,7 +6,7 @@ use maiq_shared::utils::time::*;
 use teloxide::Bot;
 use tokio::time::sleep;
 
-use crate::{bot::notifier, db::MongoPool};
+use crate::{bot::notifier::notify_update, db::MongoPool};
 
 pub struct Poller {
   bot: Bot,
@@ -58,7 +58,7 @@ impl Poller {
       }
     };
 
-    if let Err(err) = notifier::notify_update(&self.bot, &self.mongo, changes, snapshot).await {
+    if let Err(err) = notify_update(&self.bot, &self.mongo, changes, snapshot).await {
       error!("An error occured while notifying users: {}", err);
     }
   }
@@ -69,7 +69,7 @@ impl Poller {
       .num_milliseconds()
       .clamp(1000 * 10, 1000 * 24 * 60 * 60) as u64;
 
-    info!("Sleeping for {}s in awaiting of next update", wait as f32 / 1000f32);
+    // info!("Sleeping for {}s in awaiting of next update", wait as f32 / 1000f32);
     sleep(Duration::from_millis(wait)).await;
   }
 }
